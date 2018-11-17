8ac55f426787fd3cfde46081476745d6e18de2d7
Matt Sorensen <sorensenmattj@gmail.com>
2018-11-17 11:01:16 +0000

    Set number of columns and rows as properties of the Board.

--- Connect4/Connect4/Board.cs
+++ Connect4/Connect4/Board.cs
@@ -10,16 +10,17 @@ namespace Connect4
     {
         public char[,] State { get; set; }
 
+        public int NumberOfColumns => 7;
+
+        public int NumberOfRows => 6;
+
         public Board()
         {
-            var numberOfColumns = 7;
-            var numberOfRows = 6;
+            State = new char[NumberOfRows, NumberOfColumns];
 
-            State = new char[numberOfRows, numberOfColumns];
-
-            for (int i = 0; i < numberOfRows; i++)
+            for (int i = 0; i < NumberOfRows; i++)
             {
-                for (int j = 0; j < numberOfColumns; j++)
+                for (int j = 0; j < NumberOfColumns; j++)
                 {
                     State[i, j] = ' ';
                 }
@@ -28,7 +29,7 @@ namespace Connect4
 
         public void AddTokenToColumn(char token, int columnIndex)
         {
-            for (int i = State.GetUpperBound(0); i >= 0; i--)
+            for (int i = NumberOfRows; i >= 0; i--)
             {
                 if (State[i, columnIndex] == ' ')
                 {
